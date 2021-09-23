ea729f5eb4e87672833ac4da5ed6c364808ae3eb
huanghai <huanghai@zngjlh.com>
2021-09-23 10:58:14 +0800

    optimization

--- app/src/main/java/com/hh/composeplayer/base/BaseInitializer.kt
+++ app/src/main/java/com/hh/composeplayer/base/BaseInitializer.kt
@@ -16,13 +16,11 @@ import org.litepal.LitePal
  * apk生成过程中class文件转换成dex文件，默认只会生成一个dex文件，单个dex文件中的方法数不能超过65536，不然编译会报错，但是我们在开发App时肯定会集成一堆库，方法数一般都是超过65536的，解决这个问题的办法就是：一个dex装不下，用多个dex来装，gradle增加一行配置：multiDexEnabled true
  * MultiDex方法数过多采用分包形式
  * litepal数据库框架
- * XXPermissions权限请求框架
  */
 class BaseInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         MultiDex.install(context)
         LitePal.initialize(context)
-        //android11新特性  后台定位权限申请中，用户必须要勾选 始终允许，只有这样后台定位权限才能申请通过。
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
